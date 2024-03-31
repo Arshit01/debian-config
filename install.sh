eb04d28c3f81b8990e34f14397b0b9855e196a6c
Arshit01 <arsh24vora@proton.me>
2024-03-31 16:39:12 +0530

    added themes and icons, updated root conflicts

--- install.sh
+++ install.sh
@@ -1,28 +1,31 @@
 #!/bin/bash
 
 # check for root
-if [ "$EUID" -ne 0 ]; then
-	echo "This script must be run as root"
-	exit 1
-fi
+checkRoot() {
+	if [ "$EUID" -eq 0 ]; then
+		return 0
+	else
+		return 1
+	fi
+}
 
 # update the machine
 upgradeSys() {
-	apt-get update && apt-get full-upgrade -y
-	apt-get autoclean -y
-	apt-get autopurge -y
+	sudo apt-get update && apt-get full-upgrade -y
+	sudo apt-get autoclean -y
+	sudo apt-get autopurge -y
 }
 
 # install required packages
 installReq() {
-	apt-get update && xargs apt-get install -y < tobeinstalled.txt
-	apt-get autopurge -y
+	sudo apt-get update && xargs apt-get install -y < tobeinstalled.txt
+	sudo apt-get autopurge -y
 }
 
 # Set custom resolution
-setResolution() {
+displayConfig() {
 	# Define GRUB settings
-	GRUB_GFXMODE="1024x768x32"
+	GRUB_GFXMODE="1152x864x32"
 	GRUB_GFXPAYLOAD_LINUX="keep"
 	GRUB_CMDLINE_LINUX_DEFAULT="quiet splash"
 
@@ -30,14 +33,14 @@ setResolution() {
 	GRUB_CONFIG="/etc/default/grub"
 	if [ -f "$GRUB_CONFIG" ]; then
 
-    # Update the GRUB settings in the configuration file
-	sed -i "/^#*GRUB_GFXMODE=/ s/.*/GRUB_GFXMODE=\"$GRUB_GFXMODE\"/" "$GRUB_CONFIG"
-	sed -i "/^#*GRUB_GFXMODE=/ s/.*/&\nGRUB_GFXPAYLOAD_LINUX=\"$GRUB_GFXPAYLOAD_LINUX\"/" "$GRUB_CONFIG"
-	sed -i "/^#*GRUB_CMDLINE_LINUX_DEFAULT=/ s/.*/GRUB_CMDLINE_LINUX_DEFAULT=\"$GRUB_CMDLINE_LINUX_DEFAULT\"/" "$GRUB_CONFIG"
+	# Update the GRUB settings in the configuration file
+	sudo sed -i "/^#*GRUB_GFXMODE=/ s/.*/GRUB_GFXMODE=\"$GRUB_GFXMODE\"/" "$GRUB_CONFIG"
+	sudo sed -i "/^#*GRUB_CMDLINE_LINUX_DEFAULT=/ s/.*/GRUB_CMDLINE_LINUX_DEFAULT=\"$GRUB_CMDLINE_LINUX_DEFAULT\"/" "$GRUB_CONFIG"
+	sudo sed -i "/^#*GRUB_GFXMODE=/ s/.*/&\nGRUB_GFXPAYLOAD_LINUX=\"$GRUB_GFXPAYLOAD_LINUX\"/" "$GRUB_CONFIG"
 
-    # Update GRUB
-	update-initramfs -u
-    update-grub
+	# Update GRUB
+	sudo update-initramfs -u
+	sudo update-grub
 
     echo "GRUB configuration updated successfully."
 	else
@@ -45,13 +48,53 @@ setResolution() {
 		exit 1
 	fi
 
-	cp -b --suffix=.bak "assets/10-monitor_120hz_refresh.conf" "/etc/X11/xorg.conf.d/"
+	# Copy display resolution config file 
+	sudo cp -b --suffix=.bak "assets/10-monitor_120hz_refresh.conf" "/etc/X11/xorg.conf.d/"
+
+	# Copy Login greeter config file
+	sudo cp -b --suffix=.bak "assets/lightdm-gtk-greeter.conf" "/etc/lightdm/"
 }
 
-setLoginDM() {
-	cp -b --suffix=.bak "assets/lightdm-gtk-greeter.conf" "/etc/lightdm/"
+allConfigSudo() {
+	# Exaract themes in themes folder
+	sudo tar --lzma -xf "assets/themes/Flat-Remix-Dark-themes.tar" "/usr/share/themes/"
+
+	# Exaract icons in icons folder
+	sudo tar --lzma -xf "assets/icons/Flat-Remix-Dark-icons.tar" "/usr/share/icons/"
+
+	# Make location bar appear in button form in thunar
+	sudo xfconf-query -c thunar -p /last-location-bar -s "ThunarLocationButtons"
+
+	# Set theme and icons
+	sudo xfconf-query -c xsettings -p /Net/ThemeName "Flat-Remix-Blue-Dark"
+	sudo xfconf-query -c xsettings -p /Net/IconThemeName "Flat-Remix-Green-Dark"
+	sudo xfconf-query -c xsettings -p /Xfce/SyncThemes -s true
 }
 
-changePanelIconSize() {
-	xfconf-query -c xfce4-panel -p /panels/panel-1/icon-size -s 25
+
+allConfigNormal() {
+	# Remove Shade icon option and scroll to rollup
+	xfconf-query -c xfwm4 -p /general/button_layout -s "O|HMC"
+	xfconf-query -c xfwm4 -p /general/mousewheel_rollup false
+
+	# Make location bar appear in button form in thunar
+	xfconf-query -c thunar -p /last-location-bar -s "ThunarLocationButtons"
+
+	# Set XFCE panel icon size 
+	xfconf-query -c xfce4-panel -p /panels/panel-1/size -s 35
+
+	# Set theme and icons
+	xfconf-query -c xfwm4 -p /general/theme "Flat-Remix-Blue-Dark"
+	xfconf-query -c xsettings -p /Net/ThemeName "Flat-Remix-Blue-Dark"
+	xfconf-query -c xsettings -p /Net/IconThemeName "Flat-Remix-Green-Dark"
+	xfconf-query -c xsettings -p /Xfce/SyncThemes -s true
 }
+
+if checkRoot; then
+	upgradeSys
+	installReq
+    displayConfig
+    allConfigSudo
+fi
+
+allConfigNormal
