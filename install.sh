31e1916766a1f9c33be410d5f6a187b92db2bcdf
Arshit01 <arsh24vora@proton.me>
2024-04-19 00:58:13 +0530

    updated install and rootInstall

--- install.sh
+++ install.sh
@@ -9,13 +9,6 @@ checkRoot() {
 	fi
 }
 
-checkDirNormal() {
-	mkdir -p "/home/$USER/.config/qterminal.org/"
-	mkdir -p "/home/$USER/.config/qt5ct/colors/"
-	mkdir -p "/home/$USER/.config/qt5ct/qss/"
-	mkdir -p "/home/$USER/.config/xfce4/xfconf/xfce-perchannel-xml/"
-}
-
 # update the machine
 upgradeSys() {
 	sudo apt-get update && apt-get full-upgrade -y
@@ -23,10 +16,28 @@ upgradeSys() {
 	sudo apt-get autopurge -y
 }
 
+# Add other sources list
+addSource() {
+	# fsearch
+	echo 'deb http://download.opensuse.org/repositories/home:/cboxdoerfer/Debian_12/ /' | sudo tee /etc/apt/sources.list.d/home:cboxdoerfer.list
+	curl -fsSL https://download.opensuse.org/repositories/home:cboxdoerfer/Debian_12/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/home_cboxdoerfer.gpg > /dev/null
+	# brave
+	sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+	echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+}
+
 # install required packages
 installReq() {
 	sudo apt-get update && xargs apt-get install -y < tobeinstalled.txt
 	sudo apt-get autopurge -y
+}
+
+checkDir() {
+	mkdir -pv "/home/$USER/.config/fsearch/"
+	mkdir -pv "/home/$USER/.config/qt5ct/{colors,qss}/"
+	mkdir -pv "/home/$USER/.config/qterminal.org/"
+	mkdir -pv "/home/$USER/.config/Thunar/"
+	mkdir -pv "/home/$USER/.config/xfce4/"
 }
 
 # Set custom resolution
@@ -36,110 +47,35 @@ displayConfig() {
 	GRUB_GFXPAYLOAD_LINUX="keep"
 	GRUB_CMDLINE_LINUX_DEFAULT="quiet splash"
 
-	# Check if GRUB configuration file exists
 	GRUB_CONFIG="/etc/default/grub"
-	if [ -f "$GRUB_CONFIG" ]; then
 
 	# Update the GRUB settings in the configuration file
 	sudo sed -i "/^#*GRUB_GFXMODE=/ s/.*/GRUB_GFXMODE=\"$GRUB_GFXMODE\"/" "$GRUB_CONFIG"
 	sudo sed -i "/^#*GRUB_CMDLINE_LINUX_DEFAULT=/ s/.*/GRUB_CMDLINE_LINUX_DEFAULT=\"$GRUB_CMDLINE_LINUX_DEFAULT\"/" "$GRUB_CONFIG"
 	sudo sed -i "/^#*GRUB_GFXMODE=/ s/.*/&\nGRUB_GFXPAYLOAD_LINUX=\"$GRUB_GFXPAYLOAD_LINUX\"/" "$GRUB_CONFIG"
-
-	# Update GRUB
-	sudo update-initramfs -u
-	sudo update-grub
-
-    echo "GRUB configuration updated successfully."
-	else
-		echo "GRUB configuration file not found."
-		exit 1
-	fi
 }
 
-copyFilesSudo() {
-	# Login greeter config file
-	sudo cp -b --suffix=.bak "assets/sda/etc/lightdm/lightdm-gtk-greeter.conf" "/etc/lightdm/"
-
-	# display resolution config file 
-	sudo cp -b --suffix=.bak "assets/sda/etc/X11/xorg.conf.d/10-monitor_120hz_refresh.conf" "/etc/X11/xorg.conf.d/"
-	
-	# XFCE Settings menu configuration
-	sudo cp -b --suffix=.bak "assets/sda/etc/xdg/menus/xfce-settings-manager.menu" "/etc/xdg/menus/"
-	
-	# Mousepad theme or styling
-	sudo cp -b --suffix=.bak "assets/sda/usr/share/gtksourceview-4/styles/Debian-Dark.xml" "/usr/share/gtksourceview-4/styles/"
-
-	# QTerminal color scheme
-	sudo cp -b --suffix=.bak "assets/sda/usr/share/qtermwidget5/color-schemes/Debian-Dark.colorscheme" "/usr/share/qtermwidget5/color-schemes/"
-
-	#TODO: add Plymouth themes
+copyFiles() {
+	# Fsearch configuration
+	cp -bv --suffix=.bak "assets/sda/home/USER/.config/fsearch/fsearch.conf" "/home/$USER/.config/fsearch/"
+	# Qt5CT configurations
+	cp -bvr --suffix=.bak "assets/sda/home/USER/.config/qt5ct/" "/home/$USER/.config/"
+	# Qterminal configuration file
+	cp -bv --suffix=.bak "assets/sda/home/USER/.config/qterminal.org/qterminal.ini" "/home/$USER/.config/qterminal.org/"
+	# Thunar configuration file
+	cp -bvr --suffix=.bak "assets/sda/home/USER/.config/Thunar/" "/home/$USER/.config/"
+    # Xfce4 configuration files
+	cp -bvr --suffix=.bak "assets/sda/home/USER/.config/xfce4/" "/home/$USER/.config/"
 }
 
-copyFilesNormal() {
-	# Copy Qt color configuration file
-	cp -b --suffix=.bak "assets/sda/home/USER/.config/qt5ct/colors/Debian-Dark.conf" "/home/$USER/.config/qt5ct/colors/"
-
-	# Copy Qt scrollbar file
-	cp -b --suffix=.bak "assets/sda/home/USER/.config/qt5ct/qss/fusion-simple-scrollbar.qss" "/home/$USER/.config/qt5ct/qss/"
-
-	# Copy Qterminal configuration file
-	cp -b --suffix=.bak "assets/sda/home/USER/.config/qterminal.org/qterminal.ini" "/home/$USER/.config/qterminal.org/"
-
-	# Copy to fix tooltip configuration
-	cp -b --suffix=.bak "assets/sda/home/USER/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-desktop.xml" "/home/$USER/.config/xfce4/xfconf/xfce-perchannel-xml/"
-}
-
-allConfigSudo() {
-	# Exaract themes in themes folder
-	sudo tar --lzma -xf "assets/sda/usr/share/themes/Flat-Remix-Dark-themes.tar" "/usr/share/themes/"
-
-	# Exaract icons in icons folder
-	sudo tar --lzma -xf "assets/sda/usr/share/icons/Flat-Remix-Dark-icons.tar" "/usr/share/icons/"
-
-	# Make location bar appear in button form in thunar
-	sudo xfconf-query -c thunar -p /last-location-bar -s "ThunarLocationButtons"
-
-	# Set theme and icons
-	sudo xfconf-query -c xsettings -p /Net/ThemeName "Flat-Remix-Green-Dark"
-	sudo xfconf-query -c xsettings -p /Net/IconThemeName "Flat-Remix-Green-Dark"
-	sudo xfconf-query -c xsettings -p /Xfce/SyncThemes -s true
-
-	# Set default terminal
-	sudo sed -i "/^TerminalEmulator=/ s/.*/TerminalEmulator=qterminal/" "/etc/xdg/xfce4/helpers.rc"
-	sudo update-alternatives --set x-terminal-emulator /usr/bin/qterminal
-}
-
-allConfigNormal() {
-	# Remove Shade icon option and scroll to rollup
-	xfconf-query -c xfwm4 -p /general/button_layout -s "O|HMC"
-	xfconf-query -c xfwm4 -p /general/mousewheel_rollup false
-
-	# Make location bar appear in button form in thunar
-	xfconf-query -c thunar -p /last-location-bar -s "ThunarLocationButtons"
-
-	# Set XFCE panel icon size 
-	xfconf-query -c xfce4-panel -p /panels/panel-1/size -s 35
-
-	# Set theme and icons
-	xfconf-query -c xfwm4 -p /general/theme "Flat-Remix-Blue-Dark"
-	xfconf-query -c xsettings -p /Net/ThemeName "Flat-Remix-Blue-Dark"
-	xfconf-query -c xsettings -p /Net/IconThemeName "Flat-Remix-Green-Dark"
-	xfconf-query -c xsettings -p /Xfce/SyncThemes -s true
-}
 
 if checkRoot; then
-	checkDirSudo
+	sudo cp "rootInstall.sh" "/root/"
+	sudo chmod +x "/root/rootInstall.sh"
 	upgradeSys
 	installReq
-    displayConfig
-    allConfigSudo
+	displayConfig
 fi
 
-checkDirNormal
-allConfigNormal
-
-
-# echo 'deb http://download.opensuse.org/repositories/home:/cboxdoerfer/Debian_12/ /' | sudo tee /etc/apt/sources.list.d/home:cboxdoerfer.list
-# curl -fsSL https://download.opensuse.org/repositories/home:cboxdoerfer/Debian_12/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/home_cboxdoerfer.gpg > /dev/null
-# sudo apt update
-# sudo apt install fsearch
+checkDir
+copyFiles
---
+++ rootInstall.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+checkDir() {
+        mkdir -pv "/etc/lightdm/"
+        mkdir -pv "/etc/X11/{xord.conf.d,Xsession.d}/"
+        mkdir -pv "/etc/xdg/{menus,qt5ct,qterminal.org,xfce4}/"
+		mkdir -pv "/etc/xdg/xfce4/xfce-perchannel-xml/"
+		mkdir -pv "/usr/local/sbin/"
+		mkdir -pv "/usr/share/backgrounds/"
+		mkdir -pv "/usr/share/fonts/truetype/FiraCode/"
+        mkdir -pv "/usr/share/gtksourceview-4/styles/"
+        mkdir -pv "/usr/share/plymouth/themes/"
+        mkdir -pv "/usr/share/polkit-1/actions/"
+        mkdir -pv "/usr/share/qt5ct/{colors,qss}/"
+        mkdir -pv "/usr/share/qtermwidget5/color-schemes/"
+        mkdir -pv "/usr/share/xfce4/terminal/colorschemes/"
+}
+
+copyFiles() {
+	# Exaract icons in icons folder
+	tar --lzma -xf "assets/sda/usr/share/icons/Flat-Remix-Dark-icons.tar" "/usr/share/icons/"
+	# Exaract themes in themes folder
+	tar --lzma -xf "assets/sda/usr/share/themes/Flat-Remix-Dark-themes.tar" "/usr/share/themes/"
+	# Login greeter config file
+	cp -bv --suffix=.bak "assets/sda/etc/lightdm/lightdm-gtk-greeter.conf" "/etc/lightdm/"
+	# Display resolution config file 
+	cp -bv --suffix=.bak "assets/sda/etc/X11/xorg.conf.d/10-monitor_120hz_refresh.conf" "/etc/X11/xorg.conf.d/"
+	# Environment variable file 
+	cp -bv --suffix=.bak "assets/sda/etc/X11/Xsession.d/90debian-themes" "/etc/X11/Xsession.d/"
+	# XFCE Settings menu configuration
+	cp -bv --suffix=.bak "assets/sda/etc/xdg/menus/xfce-settings-manager.menu" "/etc/xdg/menus/"
+	# Qt5 configuration settings
+	cp -bv --suffix=.bak "assets/sda/etc/xdg/qt5ct/qt5ct.conf" "/etc/xdg/qt5ct/"
+	# Qterminal configuration file
+	cp -bv --suffix=.bak "assets/sda/etc/xdg/qterminal.org/qterminal.ini" "/etc/xdg/qterminal.org/"
+	# XFCE configurations in XML format
+	cp -bvr --suffix=.bak "assets/sda/etc/xdg/xfce4/" "/etc/xdg/"
+	# Display VPN IP address in panel
+	cp -bv --suffix=.bak "assets/sda/usr/local/sbin/xfce4-panel-genmon-vpnip.sh" "/usr/local/sbin/"
+	# Debian wallpaper
+	cp -bv --suffix=.bak "assets/sda/usr/share/backgrounds/Debian.jpg" "/usr/share/backgrounds/"
+	# FiraCode Nerd fonts
+	cp -bvr --suffix=.bak "assets/sda/usr/share/fonts/truetype/FiraCode/" "/usr/share/fonts/truetype/"
+	# Mousepad theme or styling
+	cp -bv --suffix=.bak "assets/sda/usr/share/gtksourceview-4/styles/Debian-Dark.xml" "/usr/share/gtksourceview-4/styles/"
+	# Whisker menu icon
+	cp -bv --suffix=.bak "assets/sda/usr/share/icons/desktop-bvase/scalable/emblems/debian-logo.svg" "/usr/share/icons/desktop-bvase/scalable/emblems/"
+	# Plymouth themes
+	tar --gzip -xf "assets/sda/usr/share/plymouth/themes/square_hud.tar.gz" "/usr/share/plymouth/themes/"
+	# Policies for Debian
+	cp -bv --suffix=.bak "assets/sda/usr/share/polkit-1/actions/org.debian.pkexec.policy" "/usr/share/polkit-1/actions/"
+	# Qt5CT configurations
+	cp -bvr --suffix=.bak "assets/sda/usr/share/qt5ct/" "/usr/share/"
+	# QTerminal color scheme
+	cp -bv --suffix=.bak "assets/sda/usr/share/qtermwidget5/color-schemes/Debian-Dark.colorscheme" "/usr/share/qtermwidget5/color-schemes/"
+	# XFCE4 Terminal color scheme
+	cp -bv --suffix=.bak "assets/sda/usr/share/xfce4/terminal/colorschemes/Debian-Dark.theme" "/usr/share/xfce4/terminal/colorschemes/"
+}
+
+allConfig() {
+	# Update icon cache
+	for theme in /usr/share/icons/Fla*; do gtk-update-icon-cache $theme; done
+	# Change desktop-base theme
+	update-alternatives --set desktop-theme /usr/share/desktop-base/homeworld-theme
+	# Set Plymouth theme
+	plymouth-set-default-theme -R square_hud 
+	# At last update the grub
+	update-initramfs -u
+	update-grub
+}
+
+echo -e "\e[38;5;14mChecking Directories...\e[0m"
+checkDir
+# Change the location of the 'debian-config' directory accordingly
+cd /home/bright_lion/debian-config/
+echo -e "\e[38;5;14mCopiying required Files...\e[0m"
+copyFiles
+echo -e "\e[38;5;10mRequired Files Copied\e[0m"
+echo -e "\e[38;5;13mSetting Other configurations\e[0m"
+allConfig
